417b8ae67292a26356e734247bbd17b5c5461940
Twocram <2002maslovartem@gmail.com>
2021-01-16 23:10:56 +0500

    inital commit

---
+++ scripts/sidebar-menu.js
@@ -0,0 +1,13 @@
+(function(){
+    const openingBtn = document.querySelector('.sidebar__hamburger');
+    const closingBtn = document.querySelector('.sidebar__close');
+    const sideBar = document.querySelector('.sidebar');
+    
+    openingBtn.addEventListener("click", function(){
+        sideBar.classList.add('sidebar--opened');
+    });
+    
+    closingBtn.addEventListener('click', function(){
+        sideBar.classList.remove('sidebar--opened');
+    })
+}())
